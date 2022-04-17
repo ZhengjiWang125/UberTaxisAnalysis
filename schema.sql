3ca7511e0cf09ab998d4f01cb578682e373b91b5
Zhengji Wang <zhengjiwang125@gmail.com>
2022-04-17 17:43:58 -0400

    create schema.sql and create a database for test

---
+++ schema.sql
@@ -0,0 +1,44 @@
+
+CREATE TABLE daily_weather
+{
+date Datetime,
+windspeed FLOAT,
+precipitation FLOAT
+}
+
+CREATE TABLE hourly_weather
+{
+date Datetime,
+windspeed FLOAT,
+precipitation FLOAT
+}
+
+CREATE TABLE yellow_taxi
+{
+pickup_time DATETIME,
+dropoff_time DATETIME,
+pickup_longitude FLOAT,
+dropoff_longitude FLOAT,
+pickup_latitude FLOAT,
+dropoff_latitude FLOAT,
+tip FLOAT,
+charge FLOAT,
+distance FLOAT
+}
+
+CREATE TABLE Uber
+{
+charge FLOAT,
+pickup_time DATETIME,
+pickup_longitude FLOAT,
+dropoff_longitude FLOAT,
+pickup_latitude FLOAT,
+dropoff_latitude FLOAT,
+distance FLOAT
+}
+
+
+
+
+
+
