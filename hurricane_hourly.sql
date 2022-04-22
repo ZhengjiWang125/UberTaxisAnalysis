13acdf6a2dfff84d6c90ffa7ea9307e0a7fa7310
ShuqingYuan <1282963680@qq.com>
2022-04-22 13:57:47 -0400

    Some corrections on database and sql

--- hurricane_hourly.sql
+++ hurricane_hourly.sql
@@ -1,16 +1,18 @@
 
 select 
+t_1.date as date,
 t_1.hour as hour,
-t_1.sum_precipitation as sum_precipitation,
-t_1.avg_windspeed as avg_windspeed,
+t_1.precipitation as precipitation,
+t_1.windspeed as windspeed,
 sum(t_2.total_trips) as total_trips
 
 from 
 
 (select 
+strftime ('%Y-%m-%d',date) as date,
 strftime ('%H',date) as hour,
-sum(precipitation) as sum_precipitation,
-avg(windspeed) as avg_windspeed
+precipitation,
+windspeed
 
 from
 weather_hourly 
@@ -19,12 +21,16 @@ where
 date between '2012-10-22' and '2012-11-06'
 
 group by 
-strftime ('%H',date) 
+strftime ('%Y-%m-%d',date),
+strftime ('%H',date),
+precipitation,
+windspeed
 )t_1
 
 left join 
 
 (select 
+strftime ('%Y-%m-%d',pickup_time) as date,
 strftime ('%H',pickup_time) as hour,
 count(*) as total_trips
 
@@ -35,11 +41,13 @@ where
 pickup_time between '2012-10-22' and '2012-11-06'
 
 group by 
-strftime ('%H',pickup_time)
+strftime ('%Y-%m-%d',pickup_time),
+strftime ('%H',pickup_time) 
 
 union 
 
 select 
+strftime ('%Y-%m-%d',pickup_time) as date,
 strftime ('%H',pickup_time) as hour,
 count(*) as total_trips
 
@@ -50,16 +58,20 @@ where
 pickup_time between '2012-10-22' and '2012-11-06'
 
 group by 
+strftime ('%Y-%m-%d',pickup_time),
 strftime ('%H',pickup_time)
 
 )t_2
 
 on 
+t_1.date = t_2.date
+and 
 t_1.hour = t_2.hour
 
 group by 
+t_1.date,
 t_1.hour,
-t_1.sum_precipitation,
-t_1.avg_windspeed
+t_1.precipitation,
+t_1.windspeed
 
 
